d52c1347c766fa907d974090d1cec2b3c47b64d6
EXTENSION <alejandra.santin22@gmail.com>
2019-05-26 12:35:57 -0500

    Clase repositorios

---
+++ Cap06/slnApp/App.Data.Repository/AppUnitofWork.cs
@@ -0,0 +1,37 @@
+﻿using App.Data.DataAccess;
+using App.Data.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Repository
+{
+    public class AppUnitofWork : IAppUnitofWork
+    {
+        private readonly DbContext _context;
+
+        public AppUnitofWork()
+        {
+            _context = new DBModel();
+
+            this.ArtistRepository = new ArtistRepository(_context);
+            this.AlbumRepository = new AlbumRepository(_context);
+        }
+        public IArtistRepository ArtistRepository { get; set; }
+        public IAlbumRepository AlbumRepository { get; set; }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ Cap06/slnApp/App.Data.Repository/ArtistRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Data.DataAccess;
+using App.Data.Repository.Interface;
+using App.Entities.Base;
+
+namespace App.Data.Repository
+{
+    public class ArtistRepository:GenericRepository<Artist>,IArtistRepository
+    {
+     
+        public ArtistRepository(DbContext context):base(context)
+        {
+          
+        }
+
+      
+    }
+}
